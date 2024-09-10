fdc9235b511027d637425c8694190d5815c7cd86
Bo <ahsaira@tutamail.com>
2024-09-10 12:33:02 -0300

    Update metamask.js

--- js/metamask.js
+++ js/metamask.js
@@ -1,12 +1,10 @@
-let provider;
-
 document.addEventListener('DOMContentLoaded', () => {
   const connectButton = document.getElementById('connectButton');
 
   connectButton.addEventListener('click', async () => {
     if (window.ethereum) {
       try {
-        provider = new ethers.providers.Web3Provider(window.ethereum);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts", []);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
